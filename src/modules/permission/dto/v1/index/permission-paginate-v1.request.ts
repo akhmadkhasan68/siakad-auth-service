3a59c3c9611ca3035bb9b458c942819c5ded4dad
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2024-07-14 21:55:56 +0700

    feat: add filter permisison role id

--- src/modules/permission/dto/v1/index/permission-paginate-v1.request.ts
+++ src/modules/permission/dto/v1/index/permission-paginate-v1.request.ts
@@ -1,3 +1,8 @@
-import { PaginateRequest } from "src/common/requests/paginate.request";
+import { IsOptional, IsUUID } from 'class-validator';
+import { PaginateRequest } from 'src/common/requests/paginate.request';
 
-export class PermissionPaginateV1RequestDto extends PaginateRequest {}
+export class PermissionPaginateV1RequestDto extends PaginateRequest {
+    @IsOptional()
+    @IsUUID('4')
+    roleId?: string;
+}
--- src/modules/permission/services/permission.service.ts
+++ src/modules/permission/services/permission.service.ts
@@ -1,43 +1,62 @@
-import { IPermission } from "src/databases/interaces/permission.interface";
-import { PermissionRepository } from "../repositories/permission.repository";
-import { Injectable } from "@nestjs/common";
-import { PermissionPaginateV1RequestDto } from "../dto/v1/index/permission-paginate-v1.request";
-import { IPaginateResponse } from "src/common/interfaces/index.interface";
-import { PaginateService } from "src/infrastructures/services/paginate.service";
+import { Injectable } from '@nestjs/common';
+import { IPaginateResponse } from 'src/common/interfaces/index.interface';
+import { IPermission } from 'src/databases/interaces/permission.interface';
+import { PaginateService } from 'src/infrastructures/services/paginate.service';
+import { PermissionPaginateV1RequestDto } from '../dto/v1/index/permission-paginate-v1.request';
+import { PermissionRepository } from '../repositories/permission.repository';
 
 @Injectable()
 export class PermissionService extends PaginateService {
-    constructor(
-        private readonly permissionRepository: PermissionRepository,
-    ) { 
+    constructor(private readonly permissionRepository: PermissionRepository) {
         super();
     }
-    
+
     async fetchPaginate(
-        payload: PermissionPaginateV1RequestDto
+        payload: PermissionPaginateV1RequestDto,
     ): Promise<IPaginateResponse<IPermission>> {
-        const query = this.permissionRepository.permissionRepository.createQueryBuilder('permission');
+        const query = this.permissionRepository.permissionRepository
+            .createQueryBuilder('permission')
+            .leftJoinAndSelect('permission.roles', 'roles');
+
+        if (payload.roleId) {
+            query.where('roles.id = :roleId', { roleId: payload.roleId });
+        }
 
         if (payload.keyword) {
             query.andWhere((qb) => {
-                qb.where('permission.name ILIKE :keyword', { keyword: `%${payload.keyword}%` })
-                    .orWhere('permission.key ILIKE :keyword', { keyword: `%${payload.keyword}%` });
+                qb.where('permission.name ILIKE :keyword', {
+                    keyword: `%${payload.keyword}%`,
+                }).orWhere('permission.key ILIKE :keyword', {
+                    keyword: `%${payload.keyword}%`,
+                });
             });
         }
 
         if (payload.sort) {
             switch (payload.sort) {
                 case 'name':
-                    query.orderBy('permission.name', this.getOrder(payload.order));
+                    query.orderBy(
+                        'permission.name',
+                        this.getOrder(payload.order),
+                    );
                     break;
                 case 'key':
-                    query.orderBy('permission.key', this.getOrder(payload.order));
+                    query.orderBy(
+                        'permission.key',
+                        this.getOrder(payload.order),
+                    );
                     break;
                 case 'description':
-                    query.orderBy('permission.description', this.getOrder(payload.order));
+                    query.orderBy(
+                        'permission.description',
+                        this.getOrder(payload.order),
+                    );
                     break;
                 default:
-                    query.orderBy('permission.createdAt', this.getOrder(payload.order));
+                    query.orderBy(
+                        'permission.createdAt',
+                        this.getOrder(payload.order),
+                    );
                     break;
             }
         }
